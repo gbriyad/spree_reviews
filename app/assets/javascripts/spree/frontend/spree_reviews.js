24ea1dc0f773fcab4cf37e62b7fef5037777a0c4
gbriyad <gbriyad@gmail.com>
2020-09-06 10:15:55 +0600

    fix js issue

--- app/assets/javascripts/spree/frontend/spree_reviews.js
+++ app/assets/javascripts/spree/frontend/spree_reviews.js
@@ -1,8 +1,8 @@
 //= require jquery.rating
 //= require spree/frontend
-//= require spree/frontend/spree_auth
+////= require spree/frontend/spree_auth
 
 // Navigating to a page with ratings via TurboLinks shows the radio buttons
-$(document).on('page:load', function () {
+Spree.ready(function () {
   $('input[type=radio].star').rating();
 });
